6627c419080e0dc72373cf0f27ba3ba27bbfe489
Jan Pluskal <jan.pluskal@gmail.com>
2017-12-18 12:15:36 +0100

    Add CookBook.DAL.Tests.

---
+++ CookBook.DAL.Tests/CookBookDbContextTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using CookBook.DAL.Entities;
+using NUnit.Framework;
+
+namespace CookBook.DAL.Tests
+{
+    [TestFixture]
+    public class CookBookDbContextTests : IDisposable
+    {
+        public CookBookDbContextTests()
+        {
+            this.DbContext = new CookBookDbContext();
+        }
+
+        public CookBookDbContext DbContext { get; set; }
+
+        [Test]
+        public void AddIngredienceTest()
+        {
+            var ingredient = new IngredientEntity
+            {
+                Name = "Ingredient name",
+                Description = "Ingredient description"
+            };
+
+            this.DbContext.Ingredients.Add(ingredient);
+            this.DbContext.SaveChanges();
+
+            var ingredientEntity = this.DbContext.Ingredients.FirstOrDefault();
+
+            Assert.NotNull(ingredientEntity);
+            Assert.AreEqual(ingredient.Name,ingredientEntity.Name);
+            Assert.AreEqual(ingredient.Description,ingredientEntity.Description);
+        }
+
+        public void Dispose()
+        {
+            DbContext?.Dispose();
+        }
+    }
+}
---
+++ CookBook.DAL.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CookBook.DAL.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CookBook.DAL.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d651d37-2bb5-4b99-9a40-df5f3e487cc6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
