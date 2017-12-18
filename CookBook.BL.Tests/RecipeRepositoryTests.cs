f85f4c7853803cb73bc74c76b1bb8c901e1a3799
Jan Pluskal <jan.pluskal@gmail.com>
2017-12-18 12:53:23 +0100

    Add RecipeRepositoryTests

---
+++ CookBook.BL.Tests/RecipeRepositoryTests.cs
@@ -0,0 +1,22 @@
+﻿using CookBook.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CookBook.BL.Tests
+{
+    [TestFixture]
+    public class RecipeRepositoryTests
+    {
+        [Test]
+        public void GetAllTest()
+        {
+            var recipeRepository = new RecipeRepository(new RecipeMapper());
+            var recipes = recipeRepository.GetAll();
+            Assert.IsEmpty(recipes);
+        }
+    }
+}
